8de20c415906ee54d1fc91f86c07ba4035e0c458
mar <mar4419440@gmail.com>
2026-08-26 15:17:25 +0300

    Update GitHub Action for Static HTML Build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  images: { unoptimized: true }, // Required for static export
+};
 
 export default nextConfig;
